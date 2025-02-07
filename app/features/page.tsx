f97ea3279df9811703fb2fc7a17fcb83c85a9ccd
Amar Koleti <akoleti@gmail.com>
2025-02-07 16:42:58 -0600

    refactored code

---
+++ app/features/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import KeyFeatures from '@/components/sections/KeyFeatures';
+import Layout from '@/components/Layout';
+
+const FeaturesPage = () => {
+    return (
+        <Layout>
+            <KeyFeatures />
+        </Layout>
+    );
+};
+
+export default FeaturesPage;
