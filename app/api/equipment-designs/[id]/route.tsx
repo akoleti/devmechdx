9430ac0d829af5070c70eec08496de236e954b77
Amar Koleti <akoleti@gmail.com>
2025-02-21 00:05:21 -0600

    fix for  next-async-request-api

--- app/api/equipment-designs/[id]/route.tsx
+++ app/api/equipment-designs/[id]/route.tsx
@@ -1,10 +1,8 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
-export async function GET(
-  request: Request,
-  { params }: { params: { id: string } }
-) {
+export async function GET(request: Request, props: { params: Promise<{ id: string }> }) {
+  const params = await props.params;
   try {
     const equipmentDesign = await prisma.equipmentDesignInfo.findUnique({
       where: {
@@ -31,10 +29,8 @@ export async function GET(
   }
 }
 
-export async function PUT(
-  request: Request,
-  { params }: { params: { id: string } }
-) {
+export async function PUT(request: Request, props: { params: Promise<{ id: string }> }) {
+  const params = await props.params;
   try {
     const body = await request.json();
     const equipmentDesign = await prisma.equipmentDesignInfo.update({
@@ -61,10 +57,8 @@ export async function PUT(
   }
 }
 
-export async function DELETE(
-  request: Request,
-  { params }: { params: { id: string } }
-) {
+export async function DELETE(request: Request, props: { params: Promise<{ id: string }> }) {
+  const params = await props.params;
   try {
     await prisma.equipmentDesignInfo.delete({
       where: {
